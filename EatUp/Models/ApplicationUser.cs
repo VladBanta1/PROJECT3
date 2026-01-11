e68411c9c993e16458985a5833d9e5debc247bf4
VladBanta1 <Banta.vlad.r2t@student.ucv.ro>
2026-01-11 14:56:30 +0200

    Add ASP.NET Identity

---
+++ EatUp/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace EatUp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+      
+        public string? DisplayName { get; set; }
+
+        public ICollection<Restaurant>? Restaurants { get; set; }
+    }
+}
